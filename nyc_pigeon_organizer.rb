f984a3156dbeb4f19702a53f86f299c8ba50139a
searching-block-4535 <juancmezat@gmail.com>
2020-11-01 20:29:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,10 +12,8 @@ def nyc_pigeon_organizer(data)
         end
         if !new_data[pigeon][attribute]
           new_data[pigeon][attribute] = []
-          if !new_data[attribute].any?(inner.to_s)
-            new_data[pigeon][attribute].push(inner.to_s)
-          end 
-          
+        end 
+        new_data[pigeon][attribute].push(inner.to_s)
         end 
         
       end   
