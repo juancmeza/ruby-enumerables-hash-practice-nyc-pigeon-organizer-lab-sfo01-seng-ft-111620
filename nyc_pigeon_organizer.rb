0ebe189dc4e78289007a3212c2f4273972f924ab
searching-block-4535 <juancmezat@gmail.com>
2020-11-01 19:53:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,22 +8,11 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, inner_att|
     inner_att.each do |inner, pigeons|
       pigeons.each do |pigeon|  
-        names.push(pigeon)
-        new_data[pigeon] = {}
-      end   
-    end
-  end
-
-  names = names.uniq 
-  binding.pry
-
-  data.each do |attribute, inner_att|
-    inner_att.each do |inner, pigeons|
-      pigeons.each do |pigeon|
-        if !pigeons.find(pigeon)
-          new_data[pigeon][attribute].push(inner_att.to_str)
+        if !new_data[pigeon]
+          new_data[pigeon] = {}
         end
-      end
+        
+      end   
     end
   end
   
